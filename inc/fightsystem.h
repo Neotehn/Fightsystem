427d4db4df8f9c568cf09fde00101352b7bcc010
hey <Neotehn@linux.fritz.box>
2021-04-16 07:28:56 -0400

    added my.h

---
+++ inc/fightsystem.h
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2021
+** my_defender
+** File description:
+** header file
+*/
+
+#ifndef FIGHTSYSTEM_H_
+#define FIGHTSYSTEM_H_
+
+#include <SFML/Audio.h>
+#include <SFML/Graphics.h>
+#include <SFML/System.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <math.h>
+
+
+#endif /* !FIGHTSYSTEM_H_ */
